633c2d76e4804e104b323c3f3c3f0790ab1144c4
foolnando <47675174+foolnando@users.noreply.github.com>
2019-04-07 11:22:05 -0400

    Add files via upload

---
+++ vetor.h
@@ -0,0 +1,30 @@
+#ifndef VETOR_H
+#define VETOR_H
+
+
+typedef struct tipoNo {
+  tipoDado d;
+  struct tipoNo *prox;
+} tipoNo;
+
+typedef struct {
+  tipoNo *prim, *ult;
+
+} tipoLista;
+
+void criarListaEncadeada(ListaEncadeada *lista); // Cria Lista encadeada
+No* CriaNo(int num); //Cria um novo no
+void CriaNoListaEncadeada(ListaEncadeada *lista, int num); //Cria um novo no na lista
+void preencheVetorOrdenado(int n, int v[]); //preenche o vetor aleatoriamente padronizada
+void preencheListaVetor (int n, tipoLista *lista,int v[]); //preenche lista e vetor com os mesmos valores
+void preencheVetorDesordenado (int n,,int v[]); //preenche o vetor de forma completamente aleatoria
+int buscaBinariaVetor(int chave, int n, int v[]); //busca binaria no vetor
+int BuscaSequencialVetor(int chave, int n, int v[]); //busca sequencial no vetor
+int BuscaSequencialListaEncadeada(int chave, tipoLista lista); //busca sequencial lista Encadeada
+void insertionSort(int  v[], int tamanho); // insertion sort 
+void bubbleSort (int vetor[], int n); //bubble sort
+void quickSort(int v[], int tamanho); //quick sort
+
+
+
+#endif
